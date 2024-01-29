7f47ff8a6f5b50a16266ef4a4be2d1ec4311e13a
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-29 20:39:57 +0300

    add model

---
+++ Movies/Movies/Models/Title.swift
@@ -0,0 +1,24 @@
+//
+//  Title.swift
+//  Movies
+//
+//  Created by Nikolai Maksimov on 29.01.2024.
+//
+
+import Foundation
+
+struct TrendingTitleResponse: Codable {
+    let results: [Title]
+}
+
+struct Title: Codable {
+    let id: Int
+    let media_type: String?
+    let original_name: String?
+    let original_title: String?
+    let poster_path: String?
+    let overview: String?
+    let vote_count: Int
+    let release_date: String?
+    let vote_average: Double
+}
