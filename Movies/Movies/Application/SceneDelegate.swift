a1b47b57121bded7a68cfd4aefad9c3a733d1fc3
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-29 20:35:35 +0300

    setup project

---
+++ Movies/Movies/Application/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Movies
+//
+//  Created by Nikolai Maksimov on 29.01.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = MainTabBarController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Movies/Movies/ViewControllers/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  Movies
+//
+//  Created by Nikolai Maksimov on 29.01.2024.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
