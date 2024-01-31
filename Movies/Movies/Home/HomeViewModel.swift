47ef66c36186025b61c7eeea1808bdac6177d38a
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-31 20:13:32 +0300

    add home view model

--- Movies/Movies/Home/HomeViewModel.swift
+++ Movies/Movies/Home/HomeViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+enum Sections: Int {
+    case TrendingMovies = 0
+    case TrendingTV = 1
+    case Popular = 2
+    case Upcoming = 3
+    case TopRated = 4
+}
+
+protocol HomeViewModelProtocol {
+    
+    func numberOfSections() -> Int
+    func fetch() -> NetworkManagerProtocol
+    //    init(networkManager: NetworkManagerProtocol)
+}
+
+final class HomeViewModel: HomeViewModelProtocol {
+    
+    private let sectionTitles: [String] = ["Trending Movies", "Trending Tv", "Popular", "Upcoming Movies", "Top Rated"]
+    private let networkManager: NetworkManagerProtocol = NetworkManager()
+    
+    //    init(networkManager: NetworkManagerProtocol) {
+    //        self.networkManager = networkManager
+    //    }
+    
+    func numberOfSections() -> Int {
+        sectionTitles.count
+    }
+    
+    func fetch() -> NetworkManagerProtocol {
+        networkManager
+    }
+}
